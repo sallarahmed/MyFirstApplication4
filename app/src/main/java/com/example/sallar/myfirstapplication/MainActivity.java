5566ca8a66560ef75b86d93f618a1f5d7a2e0447
sallar <sallarahmed@live.com>
2018-04-07 03:32:13 +0500

    jhk

---
+++ app/src/main/java/com/example/sallar/myfirstapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.sallar.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "HEllo", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        
+    }
+}
